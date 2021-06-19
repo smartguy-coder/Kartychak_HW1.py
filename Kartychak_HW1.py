f2f8e6e4c2c2ab15c3b10ed0d4e38922a331c66c
Василий <w.i.k.mailua@gmail.com>
2021-06-19 14:18:31 +0300

    STEP 4: third commit branch master - final task 2

--- Kartychak_HW1.py
+++ Kartychak_HW1.py
@@ -1,3 +1,4 @@
 print('STEP 1: Create the project and locate on GIT')
 print('STEP 2: first commit branch master')
 print('STEP 3: second commit branch master')
+print('STEP 4: third commit branch master - final task 2')
