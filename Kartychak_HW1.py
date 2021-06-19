e5ded9ba8bd1b970d457b70655998bacc695c184
Василий <w.i.k.mailua@gmail.com>
2021-06-19 14:31:57 +0300

    STEP 5: first commit branch develop

--- Kartychak_HW1.py
+++ Kartychak_HW1.py
@@ -2,3 +2,6 @@ print('STEP 1: Create the project and locate on GIT')
 print('STEP 2: first commit branch master')
 print('STEP 3: second commit branch master')
 print('STEP 4: third commit branch master - final task 2')
+
+# task 3 - branch develop
+print('STEP 5: first commit branch develop')
